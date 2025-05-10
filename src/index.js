227c80da824a1bd3b2f8eec5d9c14097358399f1
Jeffrey <j.lo128456@gmail.com>
2025-05-10 15:34:10 +1000

    update App.css and remove index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.js';
-import './index.css';
+import './components/App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
